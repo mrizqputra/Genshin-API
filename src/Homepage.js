0f6067955d69397a6698daadb3e1a2f3101530b9
mrizqputra <rizkysapp@gmail.com>
2022-12-28 14:41:50 +0700

    change homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,10 +1,119 @@
-import { getValue } from "@testing-library/user-event/dist/utils";
-import GetCharacter from "./API/GetCharacter";
-import { React, useState } from "react";
+// import { getValue } from "@testing-library/user-event/dist/utils";
+// import GetCharacter from "./API/GetCharacter";
+import { React, useState, useEffect } from "react";
 import axios from "axios";
 
 function HomePage() {
-  const character = GetCharacter(getValue);
+  const [charactersList, setCharactersList] = useState([]);
+
+  useEffect(() => {
+    // function to get all characters
+    axios({
+      method: "GET",
+      url: `https://genshin-db-api.vercel.app/api/characters`,
+      params: {
+        query: "names",
+        // dumpResult: false, // The query result will return an object with the properties: { query, folder, match, matchtype, options, filename, result }.
+        // matchNames: false, // Allows the matching of names.
+        // matchAltNames: true, // Allows the matching of alternate or custom names.
+        // matchAliases: false, // Allows the matching of aliases. These are searchable fields that returns the data object the query matched in.
+        matchCategories: true, // Allows the matching of categories. If true, then returns an array if it matches.
+        // verboseCategories: false, // Used if a category is matched. If true, then replaces each string name in the array with the data object instead.
+        // queryLanguages: ["English"], // Array of languages that your query will be searched in.
+        resultLanguage: "English", // Output language that you want your results to be in.
+      },
+      headers: {},
+    })
+      .then(function (response) {
+        console.log(response);
+        setCharactersList(response.data);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }, []);
+
+  const getAllCharacters = () => {
+    // function to get all characters
+    axios({
+      method: "GET",
+      url: `https://genshin-db-api.vercel.app/api/characters`,
+      params: {
+        query: "names",
+        // dumpResult: false, // The query result will return an object with the properties: { query, folder, match, matchtype, options, filename, result }.
+        // matchNames: false, // Allows the matching of names.
+        // matchAltNames: true, // Allows the matching of alternate or custom names.
+        // matchAliases: false, // Allows the matching of aliases. These are searchable fields that returns the data object the query matched in.
+        matchCategories: true, // Allows the matching of categories. If true, then returns an array if it matches.
+        // verboseCategories: false, // Used if a category is matched. If true, then replaces each string name in the array with the data object instead.
+        // queryLanguages: ["English"], // Array of languages that your query will be searched in.
+        resultLanguage: "English", // Output language that you want your results to be in.
+      },
+      headers: {},
+    })
+      .then(function (response) {
+        console.log(response);
+        setCharactersList(response.data);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  };
+
+  const get5StarCharacters = () => {
+    // function to get all characters
+    axios({
+      method: "GET",
+      url: `https://genshin-db-api.vercel.app/api/characters`,
+      params: {
+        query: "5",
+        // dumpResult: false, // The query result will return an object with the properties: { query, folder, match, matchtype, options, filename, result }.
+        // matchNames: false, // Allows the matching of names.
+        // matchAltNames: true, // Allows the matching of alternate or custom names.
+        // matchAliases: false, // Allows the matching of aliases. These are searchable fields that returns the data object the query matched in.
+        matchCategories: true, // Allows the matching of categories. If true, then returns an array if it matches.
+        // verboseCategories: false, // Used if a category is matched. If true, then replaces each string name in the array with the data object instead.
+        // queryLanguages: ["English"], // Array of languages that your query will be searched in.
+        resultLanguage: "English", // Output language that you want your results to be in.
+      },
+      headers: {},
+    })
+      .then(function (response) {
+        console.log(response);
+        setCharactersList(response.data);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  };
+
+  const get4StarCharacters = () => {
+    // function to get all characters
+    axios({
+      method: "GET",
+      url: `https://genshin-db-api.vercel.app/api/characters`,
+      params: {
+        query: "4",
+        // dumpResult: false, // The query result will return an object with the properties: { query, folder, match, matchtype, options, filename, result }.
+        // matchNames: false, // Allows the matching of names.
+        // matchAltNames: true, // Allows the matching of alternate or custom names.
+        // matchAliases: false, // Allows the matching of aliases. These are searchable fields that returns the data object the query matched in.
+        matchCategories: true, // Allows the matching of categories. If true, then returns an array if it matches.
+        // verboseCategories: false, // Used if a category is matched. If true, then replaces each string name in the array with the data object instead.
+        // queryLanguages: ["English"], // Array of languages that your query will be searched in.
+        resultLanguage: "English", // Output language that you want your results to be in.
+      },
+      headers: {},
+    })
+      .then(function (response) {
+        console.log(response);
+        setCharactersList(response.data);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  };
+
   const [characterDetail, setCharacterDetail] = useState(null);
 
   const getCharacterDetail = (value) => {
@@ -71,7 +180,59 @@ function HomePage() {
       <div className="container mb-10">
         <div className="flex flex-wrap">
           <div className="md:w-1/4 border-r-2 border-blue-900">
-            {character.map((value) => {
+            <div className="flex flex-wrap place-content-around">
+              <button
+                className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900"
+                onClick={() => getAllCharacters()}
+              >
+                All
+              </button>
+              <button
+                className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900"
+                onClick={() => get5StarCharacters()}
+              >
+                <div className="flex flex-wrap">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                    />
+                  </svg>
+                  5
+                </div>
+              </button>
+              <button
+                className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900"
+                onClick={() => get4StarCharacters()}
+              >
+                <div className="flex flex-wrap">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                    />
+                  </svg>
+                  4
+                </div>
+              </button>
+            </div>
+            {charactersList.map((value) => {
               return (
                 <div className="columns-1 text-center">
                   <button
