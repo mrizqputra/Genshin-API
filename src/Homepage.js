76065a6a4814ece323e080c51e10d45ec1a9799b
mrizqputra <rizkysapp@gmail.com>
2023-01-11 11:55:52 +0700

    change

--- src/Homepage.js
+++ src/Homepage.js
@@ -6,6 +6,7 @@ import Sticker3 from "./img/sticker_3.png";
 function HomePage() {
   const [materialSchedule, setMaterialSchedule] = useState([]);
   const [domainSchedule, setDomainSchedule] = useState([]);
+  // const [talentToday, setTalentToday] = useState(new Map())
 
   console.log(materialSchedule);
   console.log(domainSchedule);
@@ -34,66 +35,76 @@ function HomePage() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  console.log(genshindb.material(materialSchedule[0]));
-  // console.log(genshindb.materials(materialSchedule[0], {matchCategories:true, verboseCategories:true}).filter(materialtype => materialtype === "Weapon Ascension Material"));
+  // const wrapTalentToday = (materialSchedule) => {
+  //   for (let i=0;i<=materialSchedule.length;i++)
+  //   setTalentToday(genshindb.materials(materialSchedule[0]));
+  // };
+
+  // WrapTalentToday();
+  // console.log(talentToday);
+
+  // console.log(genshindb.materials('talent material', { matchCategories: true, verboseCategories: true }).filter());
+  // console.log(genshindb.materials(materialSchedule[12], {matchNames: true}));
+  // console.log(genshindb.materials(getToday, { matchCategories: true }).filter(materialSchedule.materialtype === 'Talent Level-Up Material'));
 
   return (
     <>
-      <section class="text-gray-600 body-font">
-        <div class="container mx-auto flex px-20 py-24 md:flex-row flex-col items-center">
-          <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+      <section className="text-gray-600 body-font">
+        <div className="container mx-auto flex px-20 py-24 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
               Before they sold out
+              {/* {wrapTalentToday(materialSchedule)} */}
             </h1>
-            <p class="mb-8 leading-relaxed">
+            <p className="mb-8 leading-relaxed">
               Copper mug try-hard pitchfork pour-over freegan heirloom neutra
               air plant cold-pressed tacos poke beard tote bag. Heirloom echo
               park mlkshk tote bag selvage hot chicken authentic tumeric
               truffaut hexagon try-hard chambray.
             </p>
-            <div class="flex justify-center">
-              <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            <div className="flex justify-center">
+              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                 Button
               </button>
-              <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
                 Button
               </button>
             </div>
           </div>
-          <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
             <img
-              class="object-cover object-center rounded"
+              className="object-cover object-center rounded"
               alt="hero"
               src={Sticker3}
             />
           </div>
         </div>
       </section>
-      <section class="text-gray-600 body-font">
-        <div class="container px-5 py-24 mx-auto">
-          <div class="flex flex-col text-center w-full mb-20">
-            <h2 class="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-20">
+            <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
               Genshin Impact Guide
             </h2>
-            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
               Welcome to genshinguide.com
             </h1>
-            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
               Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
               gentrify, subway tile poke farm-to-table. Franzen you probably
               haven't heard of them man bun deep jianbing selfies heirloom prism
               food truck ugh squid celiac humblebrag.
             </p>
           </div>
-          <div class="flex flex-wrap">
-            <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-              <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
+          <div className="flex flex-wrap">
+            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+              <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
                 Talent Material Today
               </h2>
-              <p class="leading-relaxed text-base mb-4">
-                {/* {genshindb.materials(materialSchedule, {matchCategories:true, verboseCategories:true}).filter(materialtype => materialtype === "Weapon Ascension Material")} */}
+              <p className="leading-relaxed text-base mb-4">
+{/* {Object.keys(materialSchedule).filter()} */}
               </p>
-              <Link to="#" class="text-indigo-500 inline-flex items-center">
+              <Link to="#" className="text-indigo-500 inline-flex items-center">
                 Learn More
                 <svg
                   fill="none"
@@ -101,21 +112,21 @@ function HomePage() {
                   stroke-linecap="round"
                   stroke-linejoin="round"
                   stroke-width="2"
-                  class="w-4 h-4 ml-2"
+                  className="w-4 h-4 ml-2"
                   viewBox="0 0 24 24"
                 >
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
               </Link>
             </div>
-            <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-              <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
+            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+              <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
                 Weapon Material Today
               </h2>
-              <div className="leading-relaxed text-base mb-4">
+              <div classNameName="leading-relaxed text-base mb-4">
                 <p>{domainSchedule.join(", ")}</p>
               </div>
-              <Link to="#" class="text-indigo-500 inline-flex items-center">
+              <Link to="#" className="text-indigo-500 inline-flex items-center">
                 Learn More
                 <svg
                   fill="none"
@@ -123,21 +134,21 @@ function HomePage() {
                   stroke-linecap="round"
                   stroke-linejoin="round"
                   stroke-width="2"
-                  class="w-4 h-4 ml-2"
+                  className="w-4 h-4 ml-2"
                   viewBox="0 0 24 24"
                 >
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
               </Link>
             </div>
-            <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-              <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
+            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+              <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
                 Weapon Material Today
               </h2>
-              <p class="leading-relaxed text-base mb-4">
+              <p className="leading-relaxed text-base mb-4">
                 {materialSchedule.join(", ")}
               </p>
-              <Link to="#" class="text-indigo-500 inline-flex items-center">
+              <Link to="#" className="text-indigo-500 inline-flex items-center">
                 Learn More
                 <svg
                   fill="none"
@@ -145,21 +156,21 @@ function HomePage() {
                   stroke-linecap="round"
                   stroke-linejoin="round"
                   stroke-width="2"
-                  class="w-4 h-4 ml-2"
+                  className="w-4 h-4 ml-2"
                   viewBox="0 0 24 24"
                 >
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
               </Link>
             </div>
-            <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-              <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
+            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+              <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
                 Talent Material Domain Today
               </h2>
-              <p class="leading-relaxed text-base mb-4">
+              <p className="leading-relaxed text-base mb-4">
                 {domainSchedule.join(", ")}
               </p>
-              <Link to="#" class="text-indigo-500 inline-flex items-center">
+              <Link to="#" className="text-indigo-500 inline-flex items-center">
                 Learn More
                 <svg
                   fill="none"
@@ -167,7 +178,7 @@ function HomePage() {
                   stroke-linecap="round"
                   stroke-linejoin="round"
                   stroke-width="2"
-                  class="w-4 h-4 ml-2"
+                  className="w-4 h-4 ml-2"
                   viewBox="0 0 24 24"
                 >
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
@@ -175,7 +186,7 @@ function HomePage() {
               </Link>
             </div>
           </div>
-          {/* <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          {/* <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
           Button
         </button> */}
         </div>
